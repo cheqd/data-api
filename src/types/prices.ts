6e6755f4c75a080ebec3e4a3caef7da5a5151483
nikola-cheqd <nikola@cheqd.io>
2022-06-24 10:12:16 +0200

    DEV-1300: Price discrepancy API.

---
+++ src/types/prices.ts
@@ -0,0 +1,51 @@
+export interface Market {
+    name: string;
+    identifier: string;
+    has_trading_incentive: boolean;
+}
+
+export interface ConvertedLast {
+    btc: number;
+    eth: number;
+    usd: number;
+}
+
+export interface ConvertedVolume {
+    btc: number;
+    eth: number;
+    usd: number;
+}
+
+export interface Ticker {
+    base: string;
+    target: string;
+    market: Market;
+    last: number;
+    volume: number;
+    converted_last: ConvertedLast;
+    converted_volume: ConvertedVolume;
+    trust_score: string;
+    bid_ask_spread_percentage: number;
+    timestamp: Date;
+    last_traded_at: Date;
+    last_fetch_at: Date;
+    is_anomaly: boolean;
+    is_stale: boolean;
+    trade_url: string;
+    token_info_url?: any;
+    coin_id: string;
+    target_coin_id: string;
+}
+
+export interface RootObject {
+    name: string;
+    tickers: Ticker[];
+}
+
+export interface Prices {
+    osmosis_osmo: number;
+    bit_mart: number;
+    osmosis_atom: number;
+    gate_io: number;
+    arbitrage_possible: boolean;
+}
