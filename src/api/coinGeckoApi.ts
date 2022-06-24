00d30bd3f6f6a475211b029f95a0e3812cc65de4
nikola-cheqd <nikola@cheqd.io>
2022-06-24 10:12:16 +0200

    DEV-1300: Price discrepancy API.

---
+++ src/api/coinGeckoApi.ts
@@ -0,0 +1,21 @@
+// import { Account, Coin } from "../types/node";
+import { RootObject } from "../types/prices";
+
+export class CoinGeckoApi {
+
+    constructor(public readonly base_coingecko_api_url: string) {
+    }
+
+    async get_coingecko_data(): Promise<RootObject> {
+        let resp = await fetch(`${this.base_coingecko_api_url}/coins/cheqd-network/tickers`);
+        let respJson: RootObject = await resp.json();
+
+        return respJson;
+    }
+
+    calculate_difference_percentage(a: number, b: number): number {
+        let difference = 100 * Math.abs( ( a - b ) / ( (a+b)/2 ) );
+        console.log(difference);
+        return difference;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,40 @@
-import { Router, Request, IHTTPMethods } from 'itty-router'
+import { Router, Request, IHTTPMethods } from "itty-router";
 import { handler as totalSupplyHandler } from "./handlers/totalSupply";
 import { handler as totalBalanceHandler } from "./handlers/totalBalance";
 import { handler as circulatingSupplyHandler } from "./handlers/circulatingSupply";
 import { handler as liquidBalanceHandler } from "./handlers/liquidBalance";
 import { handler as vestingBalanceHandler } from "./handlers/vestingBalance";
 import { handler as vestedBalanceHandler } from "./handlers/vestedBalance";
-import { handler as delegatorCount } from './handlers/delegatorCount';
-import { handler as totalDelegators } from './handlers/totalDelegators';
+import { handler as delegatorCount } from "./handlers/delegatorCount";
+import { handler as totalDelegators } from "./handlers/totalDelegators";
 import { handler as totalStakedCoins } from "./handlers/totalStakedCoins";
+import { handler as coinGeckoApi } from "./handlers/coinPrice";
 
-addEventListener('fetch', (event: FetchEvent) => {
-	const router = Router<Request, IHTTPMethods>()
-	registerRoutes(router);
+addEventListener("fetch", (event: FetchEvent) => {
+  const router = Router<Request, IHTTPMethods>();
+  registerRoutes(router);
 
-	event.respondWith(router.handle(event.request).catch(handleError))
-})
+  event.respondWith(router.handle(event.request).catch(handleError));
+});
 
 function registerRoutes(router: Router) {
-	router.get('/', totalSupplyHandler);
-	router.get('/supply/total', totalSupplyHandler);
-	router.get('/supply/circulating', circulatingSupplyHandler);
-	router.get('/supply/staked', totalStakedCoins);
-	router.get('/balances/total/:address', totalBalanceHandler);
-	router.get('/balances/liquid/:address', liquidBalanceHandler);
-	router.get('/balances/vesting/:address', vestingBalanceHandler);
-	router.get('/balances/vested/:address', vestedBalanceHandler);
-	router.get('/staking/delegators/total', totalDelegators);
-	router.get('/staking/delegators/:validator_address', delegatorCount);
+  router.get("/", totalSupplyHandler);
+  router.get("/supply/total", totalSupplyHandler);
+  router.get("/supply/circulating", circulatingSupplyHandler);
+  router.get("/supply/staked", totalStakedCoins);
+  router.get("/balances/total/:address", totalBalanceHandler);
+  router.get("/balances/liquid/:address", liquidBalanceHandler);
+  router.get("/balances/vesting/:address", vestingBalanceHandler);
+  router.get("/balances/vested/:address", vestedBalanceHandler);
+  router.get("/staking/delegators/total", totalDelegators);
+  router.get("/staking/delegators/:validator_address", delegatorCount);
+  router.get("/supply/staked", totalStakedCoins);
+  router.get("/prices", coinGeckoApi);
 
-	// 404 for all other requests
-	router.all('*', () => new Response('Not Found.', { status: 404 }))
+  // 404 for all other requests
+  router.all("*", () => new Response("Not Found.", { status: 404 }));
 }
 
 function handleError(error: Error): Response {
-	return new Response(error.message || 'Server Error', { status: 500 })
+  return new Response(error.message || "Server Error", { status: 500 });
 }
