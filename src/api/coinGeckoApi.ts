4698e62bea96408cd4bfa9ac20e79b0057d972b1
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2022-06-28 13:16:19 +0530

    fix: use forEach instead of for loop in Prices API
    Signed-off-by: jay-dee7 <jasdeepsingh.uppal@gmail.com>

--- src/api/coinGeckoApi.ts
+++ src/api/coinGeckoApi.ts
@@ -3,37 +3,37 @@ import { RootObject } from "../types/prices";
 
 export class CoinGeckoApi {
 
-    constructor(public readonly base_coingecko_api_url: string) {
-    }
+	constructor(public readonly base_coingecko_api_url: string) {
+	}
 
-    async get_coingecko_data(): Promise<RootObject> {
-        let resp = await fetch(`${this.base_coingecko_api_url}/coins/cheqd-network/tickers`);
-        let respJson = await resp.json() as RootObject;
+	async get_coingecko_data(): Promise<RootObject> {
+		let resp = await fetch(`${this.base_coingecko_api_url}/coins/cheqd-network/tickers`);
+		let respJson = await resp.json() as RootObject;
 
-        return respJson;
-    }
+		return respJson;
+	}
 
-    calculate_difference_percentage(a: number, b: number): number {
-        return 100 * Math.abs( ( a - b ) / ( (a+b)/2 ) );
-    }
+	calculate_difference_percentage(a: number, b: number): number {
+		return 100 * Math.abs((a - b) / ((a + b) / 2));
+	}
 
-    arbitrage_opportunity(bit_mart: number, gate_io: number, osmosis_atom: number, osmosis_osmo: number): boolean {
-        const arbitrage_percentage = 4;
-        let arbitrage_opportunity = false;
+	arbitrage_opportunity(bit_mart: number, gate_io: number, osmosis_atom: number, osmosis_osmo: number): boolean {
+		const arbitrage_percentage = 4;
+		let arbitrage_opportunity = false;
 
-        if (
-            this.calculate_difference_percentage(bit_mart, gate_io) > arbitrage_percentage ||
-            this.calculate_difference_percentage(bit_mart, osmosis_atom) > arbitrage_percentage ||
-            this.calculate_difference_percentage(bit_mart, osmosis_osmo) > arbitrage_percentage ||
-            this.calculate_difference_percentage(gate_io, osmosis_atom) > arbitrage_percentage ||
-            this.calculate_difference_percentage(gate_io, osmosis_osmo) > arbitrage_percentage ||
-            this.calculate_difference_percentage(osmosis_atom, osmosis_osmo) > arbitrage_percentage) {
-                arbitrage_opportunity = true
-        } else { 
-            arbitrage_opportunity = false
-        }
+		if (
+			this.calculate_difference_percentage(bit_mart, gate_io) > arbitrage_percentage ||
+			this.calculate_difference_percentage(bit_mart, osmosis_atom) > arbitrage_percentage ||
+			this.calculate_difference_percentage(bit_mart, osmosis_osmo) > arbitrage_percentage ||
+			this.calculate_difference_percentage(gate_io, osmosis_atom) > arbitrage_percentage ||
+			this.calculate_difference_percentage(gate_io, osmosis_osmo) > arbitrage_percentage ||
+			this.calculate_difference_percentage(osmosis_atom, osmosis_osmo) > arbitrage_percentage) {
+			arbitrage_opportunity = true
+		} else {
+			arbitrage_opportunity = false
+		}
 
-        return arbitrage_opportunity;
-    }
+		return arbitrage_opportunity;
+	}
 }
 
--- src/handlers/coinPrice.ts
+++ src/handlers/coinPrice.ts
@@ -3,34 +3,32 @@ import { Prices } from "../types/prices";
 import { CoinGeckoApi } from "../api/coinGeckoApi";
 
 export async function handler(request: Request): Promise<Response> {
-    let coinGeckoApi = new CoinGeckoApi(COINGECKO_API);
-    let coingecko = await coinGeckoApi.get_coingecko_data();
-    let prices = {} as Prices;
+	let coinGeckoApi = new CoinGeckoApi(COINGECKO_API);
+	let coingecko = await coinGeckoApi.get_coingecko_data();
+	let prices = {} as Prices;
 
-    for (let i = 0; i < coingecko.tickers.length; i++) {
-        var item = coingecko.tickers[i].market.name;
-        switch(item) {
-            case 'BitMart':
-                prices.bit_mart = coingecko.tickers[i].converted_last.usd;
-            break;
-            case 'Gate.io':
-                prices.gate_io = coingecko.tickers[i].converted_last.usd;
-            break;
-            case 'Osmosis':
-                if (coingecko.tickers[i].target_coin_id === "osmosis" && coingecko.tickers[i].coin_id === "cheqd-network") {
-                    prices.osmosis_osmo = coingecko.tickers[i].converted_last.usd;
-                } else if (coingecko.tickers[i].target_coin_id === "cheqd-network" && coingecko.tickers[i].coin_id === "cosmos") {
-                    prices.osmosis_atom = coingecko.tickers[i].converted_last.usd;
-                }
-            break;
-        }
-    }
+	coingecko.tickers.forEach((ticker) => {
+		switch (ticker.market.name) {
+			case 'BitMart':
+				prices.bit_mart = ticker.converted_last.usd;
+				break;
+			case 'Gate.io':
+				prices.gate_io = ticker.converted_last.usd;
+				break;
+			case 'Osmosis':
+				if (ticker.target_coin_id === "osmosis" && ticker.coin_id === "cheqd-network") {
+					prices.osmosis_osmo = ticker.converted_last.usd;
+				} else if (ticker.target_coin_id === "cheqd-network" && ticker.coin_id === "cosmos") {
+					prices.osmosis_atom = ticker.converted_last.usd;
+				}
+				break;
+		}
+	})
 
-    prices.arbitrage_opportunity = coinGeckoApi.arbitrage_opportunity(prices.bit_mart, prices.gate_io, prices.osmosis_atom, prices.osmosis_osmo);
-    
-    return new Response(JSON.stringify(prices), {
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-        },
-      });
+	prices.arbitrage_opportunity = coinGeckoApi.arbitrage_opportunity(prices.bit_mart, prices.gate_io, prices.osmosis_atom, prices.osmosis_osmo);
+	return new Response(JSON.stringify(prices), {
+		headers: {
+			'content-type': 'application/json;charset=UTF-8',
+		},
+	});
 }
