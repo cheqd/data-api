60d9cd60bb7cd3db1066a348df346fc150c46982
nikola-cheqd <nikola@cheqd.io>
2022-08-05 15:21:45 +0200

    DEV-1300: Dynamic CoinGecko marketplaces.

--- src/handlers/coinPrice.ts
+++ src/handlers/coinPrice.ts
@@ -1,14 +1,13 @@
-import { Request } from "itty-router";
 import { Price, Payload } from "../types/prices";
 import { CoinGeckoApi } from "../api/coinGeckoApi";
 
 export async function handler(request: Request): Promise<Response> {
-	let coinGeckoApi = new CoinGeckoApi("https://api.coingecko.com/api/v3");
+	let coinGeckoApi = new CoinGeckoApi(COINGECKO_API);
 	let coingecko = await coinGeckoApi.get_coingecko_data();
 	var prices = new Array<Price>();
 	var arbitrage_opportunity: boolean;
 
-	coingecko.tickers.forEach((ticker, index) => {
+	coingecko.tickers.forEach((ticker) => {
 		let coin_pair: Price = {
 			market: ticker.market.name,
 			coin_pair: ticker.coin_id === "cheqd-network" ? ticker.target_coin_id : ticker.coin_id,
