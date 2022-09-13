cabbc0d9406a6c67489ce4975d1c2efcd4a0e870
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-09-14 00:59:33 +0100

    Pass token exponent

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -1,8 +1,8 @@
-export {}
-
 declare global {
   const TOKEN_EXPONENT: number;
   const REST_API: string;
   const GRAPHQL_API: string;
   const CIRCULATING_SUPPLY_WATCHLIST: KVNamespace;
 }
+
+export {}
