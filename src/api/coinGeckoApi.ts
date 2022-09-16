0f30ef0af938820c770b59e57c8227324c5141a2
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-09-16 21:07:31 +0300

    chore: proof of concept

--- src/api/coinGeckoApi.ts
+++ src/api/coinGeckoApi.ts
@@ -1,35 +1,58 @@
-import { Price, RootObject } from "../types/prices";
+import { ArbitrageOpportunity, Price, RootObject } from "../types/prices";
 
 export class CoinGeckoApi {
+  constructor(public readonly base_coingecko_api_url: string) {}
 
-	constructor(public readonly base_coingecko_api_url: string) {
-	}
+  async get_coingecko_data(): Promise<RootObject> {
+    let respJson: RootObject = {
+      name: "",
+      tickers: [],
+    };
+    try {
+      let resp = await fetch(
+        `${this.base_coingecko_api_url}/coins/cheqd-network/tickers`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-	async get_coingecko_data(): Promise<RootObject> {
-		let resp = await fetch(`${this.base_coingecko_api_url}/coins/cheqd-network/tickers`);
-		let respJson = await resp.json() as RootObject;
+      //   respJson = (await resp.json()) as RootObject;
+      respJson = (await resp.json()) as RootObject;
 
-		return respJson;
-	}
+      console.log("coingecko data", respJson);
+    } catch (err) {
+      console.log(`error in coingeco `);
+      console.log(err);
+    }
+    return respJson;
+  }
 
-	calculate_difference_percentage(a: number, b: number): number {
-		return 100 * Math.abs((a - b) / ((a + b) / 2));
-	}
+  calculate_difference_percentage(a: number, b: number): number {
+    return 100 * Math.abs((a - b) / ((a + b) / 2));
+  }
 
-	arbitrage_opportunity(prices: Price[]): boolean {
-		const arbitrage_percentage = 4;
-		var arbitrage_opportunity = false;
+  arbitrage_opportunities(prices: Price[]): ArbitrageOpportunity[] {
+    const arbitrage_percentage = 4; //TODO: change tis using ENVvar bc were going to use different for differnt env
+    var arbitrage_opportunities: ArbitrageOpportunity[] = [];
 
-		for (let i=0; i<prices.length; i++) {
-			for (let j=i+1; j<prices.length; j++) {
-				if (this.calculate_difference_percentage(prices[i].price, prices[j].price) > arbitrage_percentage) {
-					arbitrage_opportunity = true;
-				} else {
-					arbitrage_opportunity = false;
-				}
-			}
-		}
-		return arbitrage_opportunity;
-	}
+    for (let i = 0; i < prices.length; i++) {
+      for (let j = i + 1; j < prices.length; j++) {
+        const percentage_delta = this.calculate_difference_percentage(
+          prices[i].price,
+          prices[j].price
+        );
+        if (percentage_delta > arbitrage_percentage) {
+          arbitrage_opportunities.push({
+            market_a: prices[i],
+            market_b: prices[j],
+            percentage_delta: percentage_delta,
+          });
+        }
+      }
+    }
+    return arbitrage_opportunities;
+  }
 }
-
--- src/types/prices.ts
+++ src/types/prices.ts
@@ -1,54 +1,61 @@
 export interface Market {
-    name: string;
-    identifier: string;
-    has_trading_incentive: boolean;
+  name: string;
+  identifier: string;
+  has_trading_incentive: boolean;
 }
 
 export interface ConvertedLast {
-    btc: number;
-    eth: number;
-    usd: number;
+  btc: number;
+  eth: number;
+  usd: number;
 }
 
 export interface ConvertedVolume {
-    btc: number;
-    eth: number;
-    usd: number;
+  btc: number;
+  eth: number;
+  usd: number;
 }
 
 export interface Ticker {
-    base: string;
-    target: string;
-    market: Market;
-    last: number;
-    volume: number;
-    converted_last: ConvertedLast;
-    converted_volume: ConvertedVolume;
-    trust_score: string;
-    bid_ask_spread_percentage: number;
-    timestamp: Date;
-    last_traded_at: Date;
-    last_fetch_at: Date;
-    is_anomaly: boolean;
-    is_stale: boolean;
-    trade_url: string;
-    token_info_url?: any;
-    coin_id: string;
-    target_coin_id: string;
+  base: string;
+  target: string;
+  market: Market;
+  last: number;
+  volume: number;
+  converted_last: ConvertedLast;
+  converted_volume: ConvertedVolume;
+  trust_score: string;
+  bid_ask_spread_percentage: number;
+  timestamp: string;
+  last_traded_at: string;
+  last_fetch_at: string;
+  is_anomaly: boolean;
+  is_stale: boolean;
+  trade_url: string;
+  token_info_url?: any;
+  coin_id: string;
+  target_coin_id: string;
 }
 
 export interface RootObject {
-    name: string;
-    tickers: Ticker[];
+  name: string;
+  tickers: Ticker[];
 }
 
 export interface Price {
-    price: number;
-    coin_pair: string;
-    market: string;
+  price: number;
+  coin_pair: string;
+  market: string;
+}
+
+export interface ArbitrageOpportunity {
+  market_a: Price;
+  market_b: Price;
+  percentage_delta: number;
 }
 
 export interface Payload {
-    markets: Price[];
-    arbitrage_oportunity: boolean;
+  markets: Price[];
+  arbitrage_oportunities: ArbitrageOpportunity[];
+  arbitrage_oportunity: boolean;
 }
