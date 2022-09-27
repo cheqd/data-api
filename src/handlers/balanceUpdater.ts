9556b6193ac0dee754ec5d1322f5b9b3cf6d62ef
drgomesp <drgomesp@gmail.com>
2022-09-27 06:24:33 -0300

    Fixed update all balances endpoint to feed KV (temporary endpoint)

--- src/handlers/balanceUpdater.ts
+++ src/handlers/balanceUpdater.ts
@@ -2,13 +2,14 @@ import { NodeApi } from "../api/nodeApi";
 import { Request } from "itty-router";
 import { validate_cheqd_address } from "../helpers/validate";
 import { updateBalance } from "../helpers/balance";
+import { updateAllBalances } from "./cron";
 
 export async function handler(request: Request): Promise<Response> {
     let node_api = new NodeApi(REST_API);
     const address = request.params?.['address'];
 
     if (!address || !validate_cheqd_address(address)) {
-        throw new Error("missing/invalid account address")
+        return await updateAllBalances({})
     }
 
     return updateBalance(node_api, address);
