39583e452b3daf0aeaf419cd19686501bf1e19aa
drgomesp <drgomesp@gmail.com>
2022-09-27 06:12:59 -0300

    Fixed balance query checks

--- src/helpers/node.ts
+++ src/helpers/node.ts
@@ -6,17 +6,17 @@ export function total_balance_ncheq(account: Account): number {
     let balance = Number(account?.accountBalance?.coins.find(c => c.denom === "ncheq")?.amount || '0');
 
     let delegations = 0;
-    if (account?.delegationBalance?.coins) {
+    if (account?.delegationBalance?.coins && account?.delegationBalance?.coins.length > 0) {
         delegations = ncheq_to_cheqd(account?.delegationBalance?.coins[0].amount);
     }
 
     let unbonding = 0;
-    if (account?.unbondingBalance?.coins) {
+    if (account?.unbondingBalance?.coins && account?.unbondingBalance?.coins.length > 0) {
         unbonding = ncheq_to_cheqd(account?.unbondingBalance?.coins[0]?.amount);
     }
 
     let rewards = 0;
-    if (account?.rewardBalance?.coins) {
+    if (account?.rewardBalance?.coins && account?.rewardBalance?.coins.length > 0) {
         rewards = ncheq_to_cheqd(account?.rewardBalance?.coins[0]?.amount);
     }
 
