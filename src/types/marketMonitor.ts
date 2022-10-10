4c17fcbd05ddb8d75be5e604bdbfbc3a710a5d32
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-10-10 21:04:55 +0100

    Make constants match

--- src/types/marketMonitor.ts
+++ src/types/marketMonitor.ts
@@ -6,14 +6,14 @@ export interface Price {
 
 export interface ArbitrageOpportunity {
   marketPairId: string;
-  marketNameA: string;
-  coinPriceA: number;
-  coinPairA: string;
-  marketNameB: string;
-  coinPriceB: number;
-  coinPairB: string;
-  arbitragePossible: boolean;
+  marketName1: string;
+  coinPair1: string;
+  coinPrice1: number;
+  marketName2: string;
+  coinPair2: string;
+  coinPrice2: number;
   percentageDelta: number;
+  arbitragePossible: boolean;
 }
 
 export interface MarketMonitorData {
