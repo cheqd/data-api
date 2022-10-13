a13332c99c2b84aeb3f44f89675956add30468df
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-10-13 12:34:32 +0300

    fix total delegations count per validatior

--- src/api/bigDipperApi.ts
+++ src/api/bigDipperApi.ts
@@ -1,13 +1,16 @@
-import { GraphQLClient } from "../helpers/graphql";
-import { Coin, ValidatorAggregateCountResponse, ValidatorDetailResponse } from "../types/node";
-import { Account } from "../types/bigDipper";
+import { GraphQLClient } from '../helpers/graphql';
+import {
+  Coin,
+  ValidatorDelegationsCountResponse,
+  ValidatorDetailResponse,
+} from '../types/node';
+import { Account } from '../types/bigDipper';
 
 export class BigDipperApi {
-    constructor(public readonly graphql_client: GraphQLClient) {
-    }
+  constructor(public readonly graphql_client: GraphQLClient) {}
 
-    async get_account(address: string): Promise<Account | null> {
-        let query = `query Account($address: String!, $where: vesting_account_bool_exp) {
+  async get_account(address: string): Promise<Account | null> {
+    let query = `query Account($address: String!, $where: vesting_account_bool_exp) {
           accountBalance: action_account_balance(address: $address) {
             coins
           }
@@ -30,67 +33,68 @@ export class BigDipperApi {
             start_time
             end_time
           }
-        }`
+        }`;
 
-        let params = {
-            address: address,
-            where: {
-                address: {
-                    _eq: address
-                }
-            }
-        }
+    let params = {
+      address: address,
+      where: {
+        address: {
+          _eq: address,
+        },
+      },
+    };
 
-        try {
-            let resp = await this.graphql_client.query<{
-                data: any, errors: any
-            }>(query, params);
+    try {
+      let resp = await this.graphql_client.query<{
+        data: any;
+        errors: any;
+      }>(query, params);
 
-            return resp.data as Account
-        } catch (e: any) {
-            console.error(new Map(e))
-            return null;
-        }
+      return resp.data as Account;
+    } catch (e: any) {
+      console.error(new Map(e));
+      return null;
     }
+  }
 
-    async get_total_supply(): Promise<Coin[]> {
-        let query = `query Supply {
+  async get_total_supply(): Promise<Coin[]> {
+    let query = `query Supply {
             supply(order_by: {height:desc} limit: 1) {
                 coins
                 height
             }
         }`;
 
-        let resp = await this.graphql_client.query<{ data: { supply: { coins: Coin[] }[] } }>(query);
+    let resp = await this.graphql_client.query<{
+      data: { supply: { coins: Coin[] }[] };
+    }>(query);
 
-        return resp.data.supply[0].coins;
-    }
+    return resp.data.supply[0].coins;
+  }
 
-    get_delegator_count_for_validator = async (address: string): Promise<Number> => {
-        let query = `query ValidatorDetails($address: String) {
-            validator(where: {validator_info: {operator_address: {_eq: $address}}}) {
-                delegations_aggregate {
-                    aggregate {
-                        count
-                    }
-                }
-            }
-        }`
-
-        const params = {
-            address: address,
+  get_delegator_count_for_validator = async (
+    address: string
+  ): Promise<Number> => {
+    let query = `query ValidatorDelegations($address: String!, $pagination: Boolean! = true) {
+        delegations: action_validator_delegations(address: $address, count_total: $pagination) {         
+          pagination
         }
+      }
+      `;
 
-        const resp = await this.graphql_client.query<ValidatorAggregateCountResponse>(query, params);
-        if (!resp.validator || !resp.validator.length) {
-            return 0;
-        }
+    const params = {
+      address: address,
+    };
 
-        return resp.validator[0].delegations_aggregate.aggregate.count;
-    }
+    const resp = await this.graphql_client.query<{
+      data: ValidatorDelegationsCountResponse;
+    }>(query, params);
 
-    get_total_delegator_count = async (): Promise<Number> => {
-        const query = `query ValidatorDetails {
+    return resp.data.delegations.pagination.total;
+  };
+
+  get_total_delegator_count = async (): Promise<Number> => {
+    const query = `query ValidatorDetails {
             validator {
                 validatorStatuses: validator_statuses(order_by: {height: desc}, limit: 1) {
                     jailed
@@ -99,29 +103,33 @@ export class BigDipperApi {
                     delegatorAddress: delegator_address
                 }
             }
-        }`
+        }`;
 
-        const resp = await this.graphql_client.query<ValidatorDetailResponse>(query);
-        const set = new Set();
-        resp.validator.forEach((obj, i) => {
-            if (!obj.validatorStatuses[0]?.jailed) {
-                obj.delegations.forEach(delegation => {
-                    set.add(delegation.delegatorAddress)
-                })
-            }
-        })
+    const resp = await this.graphql_client.query<ValidatorDetailResponse>(
+      query
+    );
+    const set = new Set();
+    resp.validator.forEach((obj, i) => {
+      if (!obj.validatorStatuses[0]?.jailed) {
+        obj.delegations.forEach((delegation) => {
+          set.add(delegation.delegatorAddress);
+        });
+      }
+    });
 
-        return set.size
-    }
+    return set.size;
+  };
 
-    get_total_staked_coins = async (): Promise<string> => {
-        let query = `query StakingInfo{
+  get_total_staked_coins = async (): Promise<string> => {
+    let query = `query StakingInfo{
             staking_pool {
                 bonded_tokens
             }
-        }`
+        }`;
 
-        const resp = await this.graphql_client.query<{ staking_pool: [ { "bonded_tokens": string } ] }>(query);
-        return resp.staking_pool[0].bonded_tokens;
-    }
+    const resp = await this.graphql_client.query<{
+      staking_pool: [{ bonded_tokens: string }];
+    }>(query);
+    return resp.staking_pool[0].bonded_tokens;
+  };
 }
