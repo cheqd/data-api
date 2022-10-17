5047784cf0a95618fb368e4dc74c016b80812215
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-10-17 17:38:27 +0300

    fix total balance

--- src/handlers/totalBalance.ts
+++ src/handlers/totalBalance.ts
@@ -4,9 +4,6 @@ import {
   validate_cheqd_address,
 } from '../helpers/validate';
 import { ncheq_to_cheq_fixed } from '../helpers/currency';
-import { BigDipperApi } from '../api/bigDipperApi';
-import { GraphQLClient } from '../helpers/graphql';
-import { total_balance_ncheq } from '../helpers/node';
 import { NodeApi } from '../api/nodeApi';
 
 export async function handler(request: Request): Promise<Response> {
@@ -19,32 +16,19 @@ export async function handler(request: Request): Promise<Response> {
   let node_api = new NodeApi(REST_API);
   let auth_account = await node_api.auth_get_account(address);
 
-  if (is_delayed_vesting_account_type(auth_account?.['@type'])) {
-    let balance = Number(
-      (await (
-        await node_api.bank_get_account_balances(address)
-      ).find((b) => b.denom === 'ncheq')?.amount) ?? '0'
-    );
-    let rewards = Number(
-      (await await node_api.distribution_get_total_rewards(address)) ?? '0'
-    );
-    let delegated = Number(
-      auth_account?.base_vesting_account?.delegated_vesting?.find(
-        (d) => d.denom === 'ncheq'
-      )?.amount ?? '0'
-    );
+  let balance = Number(
+    (await (
+      await node_api.bank_get_account_balances(address)
+    ).find((b) => b.denom === 'ncheq')?.amount) ?? '0'
+  );
+  let rewards = Number(
+    (await await node_api.distribution_get_total_rewards(address)) ?? '0'
+  );
+  let delegated = Number(
+    auth_account?.base_vesting_account?.delegated_vesting?.find(
+      (d) => d.denom === 'ncheq'
+    )?.amount ?? '0'
+  );
 
-    return new Response(ncheq_to_cheq_fixed(balance + rewards + delegated));
-  }
-
-  let gql_client = new GraphQLClient(GRAPHQL_API);
-  let bd_api = new BigDipperApi(gql_client);
-
-  let account = await bd_api.get_account(address);
-
-  if (!account) {
-    throw new Error('Account not found');
-  }
-
-  return new Response(ncheq_to_cheq_fixed(total_balance_ncheq(account)));
+  return new Response(ncheq_to_cheq_fixed(balance + rewards + delegated));
 }
