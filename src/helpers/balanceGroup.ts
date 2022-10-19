2597392cd2e8fae49125dc07cd66182d7d922dc0
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-10-19 14:43:18 +0300

    remove :delayed

--- src/helpers/balanceGroup.ts
+++ src/helpers/balanceGroup.ts
@@ -5,10 +5,6 @@ export function extract_group_number_and_address(key: string) {
   const parts = key.split(':');
   let addr = parts[1];
   let grpN = Number(parts[0].split('_')[1]);
-
-  if (key.includes('delayed:')) {
-    addr = parts[2];
-  }
   return {
     address: addr,
     groupNumber: grpN,
