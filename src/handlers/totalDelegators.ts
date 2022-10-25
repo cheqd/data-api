3b62ffff7fa0698ade2f95193e75988835dd416e
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-10-25 16:53:11 +0300

    make total delegators count read from cache if avaliable

--- src/handlers/totalDelegators.ts
+++ src/handlers/totalDelegators.ts
@@ -1,8 +1,18 @@
 import { Request } from 'itty-router';
 import { BigDipperApi } from '../api/bigDipperApi';
 import { GraphQLClient } from '../helpers/graphql';
+import { getCachedTotalDelegatorsCount } from '../helpers/totalDelegators';
 
 export async function handler(request: Request): Promise<Response> {
+  const total_delegators_from_cache = await getCachedTotalDelegatorsCount();
+  if (
+    total_delegators_from_cache !== null &&
+    total_delegators_from_cache.totalDeleagatorsCount !== undefined
+  ) {
+    return new Response(
+      JSON.stringify(total_delegators_from_cache.totalDeleagatorsCount)
+    );
+  }
   let gql_client = new GraphQLClient(GRAPHQL_API);
   let bd_api = new BigDipperApi(gql_client);
 
