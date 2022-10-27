528a26890d9cf8abcba6ba6509d1a760a855c067
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-10-27 14:33:27 +0300

    add a logic to add and remove validators from kv

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -3,6 +3,7 @@ declare global {
   const REST_API: string;
   const GRAPHQL_API: string;
   const CIRCULATING_SUPPLY_WATCHLIST: KVNamespace;
+  const ACTIVE_VALIDATORS: KVNamespace;
   const CIRCULATING_SUPPLY_GROUPS: number;
   const MARKET_MONITORING_API: string;
   const WEBHOOK_URL: string;
--- src/helpers/totalDelegators.ts
+++ src/helpers/totalDelegators.ts
@@ -1,76 +1,92 @@
-import { BigDipperApi } from '../api/bigDipperApi';
-import { NodeApi } from '../api/nodeApi';
-import {
-  ActiveValidatorsResponse,
-  CachedTotalDelegatorsCount,
-} from '../types/node';
-import { LATEST_TOTAL_DELEGATORS_COUNT } from './constants';
-import { GraphQLClient } from './graphql';
+import { ActiveValidatorsResponse } from '../types/node';
+import { extract_group_number_and_address } from './balanceGroup';
 
-export async function cacheDelegatorsCount(totalDeleagatorsCount: Number) {
-  const data = {
-    totalDelegatorsCount: totalDeleagatorsCount,
-    updatedAt: new Date().toUTCString(),
-  };
-  await TOTAL_DELEGATORS_COUNT_ACROSS_EVERY_VALIDATOR.put(
-    LATEST_TOTAL_DELEGATORS_COUNT,
-    JSON.stringify(data)
-  );
-}
-
-export async function getCachedTotalDelegatorsCount(): Promise<CachedTotalDelegatorsCount | null> {
-  try {
-    let value = await TOTAL_DELEGATORS_COUNT_ACROSS_EVERY_VALIDATOR.get(
-      LATEST_TOTAL_DELEGATORS_COUNT
+export async function add_new_active_validators_to_kv(
+  data: ActiveValidatorsResponse
+) {
+  const latest_active_validators_from_api = data.validator_info;
+  const active_validators_from_kv = await ACTIVE_VALIDATORS.list();
+  const active_validators_from_kv_hashmap =
+    create_hashmap_of_validators_addresses_from_kv(
+      active_validators_from_kv.keys as []
     );
 
-    return JSON.parse(value!!) as CachedTotalDelegatorsCount;
-  } catch (e: any) {
-    console.error(`getCachedTotalDelegatorsCount: ${e}`);
-    return null;
-  }
-}
+  for (let latest_active_validator of latest_active_validators_from_api) {
+    // if latest_active_validator is in kv, keep it.
+    // if latest_active_validator is not in kv, add it.
+    const is_active_validator_in_kv =
+      active_validators_from_kv_hashmap.key ===
+      latest_active_validator.operator_address
+        ? true
+        : false;
 
-export async function updateTotalDelegatorsCount() {
-  let gql_client = new GraphQLClient(GRAPHQL_API);
-  let bd_api = new BigDipperApi(gql_client);
-
-  const delegators = await bd_api.get_active_validators();
-  const delegators_count = await getUniqueDelegatorsCountAccrossNetwork(
-    delegators
-  );
-  await cacheDelegatorsCount(delegators_count);
-}
-
-export async function getUniqueDelegatorsCountAccrossNetwork(
-  activeValidators: ActiveValidatorsResponse
-): Promise<number> {
-  const store = [];
-  const uniques = new Set();
-  for (let i = 0; i < activeValidators.validator_info.length; i++) {
-    const operator_address =
-      activeValidators.validator_info[i].operator_address;
-
-    const resp = await new NodeApi(
-      REST_API
-    ).staking_get_delegators_per_validator(operator_address);
-    console.log('first operator', resp);
-    store.push({
-      validator: operator_address,
-      delegators: resp.delegation_responses,
-    });
-    console.log(`At ${i}fetched delegators for validator ${operator_address}`);
-  }
-
-  console.log('done gettin delegators for each validators');
-
-  for (let i = 0; i < store.length; i++) {
-    const delegators = store[i].delegators;
-    for (let j = 0; j < delegators.length; j++) {
-      uniques.add(
-        `${delegators[j].delegation.delegator_address}${delegators[j].delegation.validator_address}`
-      );
+    if (!is_active_validator_in_kv) {
+      add_new_active_validator_in_kv(latest_active_validator.operator_address);
     }
   }
-  return uniques.size;
+}
+
+export async function remove_any_jailed_validators_from_kv(
+  active_validators: ActiveValidatorsResponse
+) {
+  // list of validators form kv
+  // list of validators from api
+  // loop throu validators from kv, and if
+  // a validator from kv doesnt exist in api remove the kv
+  const active_validators_from_kv = await ACTIVE_VALIDATORS.list();
+  const active_validators_from_api_hash_map =
+    create_hashmap_of_validators_addresses_from_api(
+      active_validators.validator_info
+    );
+
+  for (let validator_from_kv of active_validators_from_kv.keys) {
+    const key_to_look_up = extract_group_number_and_address(
+      validator_from_kv.name
+    ).address;
+    if (active_validators_from_api_hash_map.key !== key_to_look_up) {
+      delete_stale_validator_from_kv(validator_from_kv.name);
+    }
+  }
+}
+
+export function create_hashmap_of_validators_addresses_from_kv(
+  validators_from_KV: string[]
+): { key: string } {
+  // keys incase contain prexifes, since they are from KV
+
+  const hashmap: { key: string } = { key: '' };
+  for (let key of validators_from_KV) {
+    const key_to_look_up = extract_group_number_and_address(key).address;
+    if (hashmap.key !== key_to_look_up) {
+      // since kv contains prefix like grp_1.. we need to extract address only
+      hashmap.key = key_to_look_up;
+    }
+  }
+  return hashmap;
+}
+
+export function create_hashmap_of_validators_addresses_from_api(
+  validators_from_api: {
+    operator_address: string;
+  }[]
+): { key: string } {
+  // keys incase contain prexifes, since they are from KV
+  const hashmap: { key: string } = { key: '' };
+  for (let validator_address of validators_from_api) {
+    if (hashmap.key !== validator_address.operator_address) {
+      // since kv contains prefix like grp_1.. we need to extract address only
+      hashmap.key = validator_address.operator_address;
+    }
+  }
+  return hashmap;
+}
+
+async function add_new_active_validator_in_kv(address: string) {
+  const data = JSON.stringify({ updatedAt: new Date().toUTCString() });
+  // for now manually put em in group 10
+  const key = `grp_10:${address}`;
+  await ACTIVE_VALIDATORS.put(key, data);
+}
+async function delete_stale_validator_from_kv(key: string) {
+  await ACTIVE_VALIDATORS.delete(key);
 }
