e7598f94c4c70c1f76c2efabf297d0530cac1905
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-10-28 10:22:19 +0300

    change delegators count endpoint to use paginated response

--- src/handlers/delegatorCount.ts
+++ src/handlers/delegatorCount.ts
@@ -1,5 +1,5 @@
 import { Request } from 'itty-router';
-import { NodeApi } from '../api/nodeApi';
+import { get_all_delegators_for_a_validator } from '../helpers/node';
 
 export async function handler(request: Request): Promise<Response> {
   const address = request.params?.['validator_address'];
@@ -8,10 +8,8 @@ export async function handler(request: Request): Promise<Response> {
     throw new Error('No address specified or wrong address format.');
   }
 
-  const resp = await new NodeApi(REST_API).staking_get_delegators_per_validator(
-    address
-  );
-  const delegators_count = resp.delegation_responses.length;
+  const resp = await get_all_delegators_for_a_validator(address);
+  const delegators_count = resp.length;
 
   return new Response(delegators_count.toString());
 }
