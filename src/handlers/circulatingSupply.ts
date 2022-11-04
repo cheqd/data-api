b3e5e5d2720950e6cb9a5577ef7dd80468714bdd
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-11-04 19:42:12 +0000

    Throw error on circulating supply

--- src/handlers/circulatingSupply.ts
+++ src/handlers/circulatingSupply.ts
@@ -2,7 +2,12 @@ import { Request } from 'itty-router';
 import { getCirculatingSupply } from '../helpers/circulating';
 
 export async function handler(request: Request): Promise<Response> {
-  let circulating_supply = await getCirculatingSupply();
-
-  return new Response(circulating_supply.toString());
+  try {
+    let circulating_supply = await getCirculatingSupply();
+    return new Response(circulating_supply.toString());
+  }
+  catch (err: any) {
+    console.log(err);
+    throw new Error(err.message);
+  }
 }
