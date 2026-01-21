5297b5e542ce85c66c22a8b4561e07f9b116b532
Filip Djokic <87134019+filipdjokic@users.noreply.github.com>
2026-01-21 10:06:03 +0100

    fix: Change fee amount column type to numeric [DEV-5668] (#399)
    fix: Change fee amount column type to numeric

---
+++ src/database/migrations/0004_sloppy_paladin.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "did_mainnet" ALTER COLUMN "amount" SET DATA TYPE numeric;--> statement-breakpoint
+ALTER TABLE "did_testnet" ALTER COLUMN "amount" SET DATA TYPE numeric;--> statement-breakpoint
+ALTER TABLE "resource_mainnet" ALTER COLUMN "amount" SET DATA TYPE numeric;--> statement-breakpoint
+ALTER TABLE "resource_testnet" ALTER COLUMN "amount" SET DATA TYPE numeric;
